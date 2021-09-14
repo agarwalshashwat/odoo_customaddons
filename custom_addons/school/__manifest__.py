86ef799ed1d5d1ead929984337912b76bbf20ddf
agarwalshashwatpheuture <88312377+agarwalshashwatpheuture@users.noreply.github.com>
2021-09-14 18:31:05 +0530

    Add files via upload

---
+++ custom_addons/school/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    "name":"School Database",
+    "version":"1.0",
+    "depends":["base"],
+    "data":[
+        "security/security.xml",
+        "security/ir.model.access.csv",
+        "views/school_view.xml",
+    ],
+    "sequence":10,
+    "installable":True,
+    "application":True,
+}
